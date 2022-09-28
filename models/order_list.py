5d9d86e4367555c08cbf63c44f1a2eeb58c8df72
Steven perry <Sperry3006@gmail.com>
2022-09-28 19:48:33 +0100

    Web app to show customer orders

---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.orders import *
+
+order1 = Order("Alan Smith", "25/7/22", 2, 653)
+order2 = Order("Maggie Smith", "23/8/20", 1, 768)
+order3 = Order("Peter Mayhew", "30/06/21", 1, 453)
+
+orders = [order1, order2, order3]
